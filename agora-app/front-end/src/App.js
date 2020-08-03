c35f18a58d83069accbd83f8eee9da31f1e7cfea
stece850 <stece850@student.otago.ac.nz>
2020-08-03 19:47:44 +1200

    created basic form for signup page. will continue tomorrow

--- agora-app/front-end/src/App.js
+++ agora-app/front-end/src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
-import Home from './screens/Home';
-import SignUp from './screens/SignUp';
-import SignIn from './screens/SignIn';
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import Home from "./screens/Home";
+import SignUp from "./screens/SignUp";
+import SignIn from "./screens/SignIn";
 
 function App() {
   return (
-      <BrowserRouter>
-        <div className="container">
-          <div className="main">
-              <Link to="/">Home</Link>
-              <Link to="/signup">Sign Up</Link>
-              <Link to="/signin">Sign In</Link>
-              <div className="content-display">
-                  <Route path="/signup" component={SignUp} />
-                  <Route path="/signin" component={SignIn} />
-                  <Route path="/" exact={true} component={Home} />
-              </div>
-            </div>
+    <BrowserRouter>
+      <div className="container">
+        <div className="main">
+          <Link to="/">Home</Link>
+          <Link to="/signup">Sign Up</Link>
+          <Link to="/signin">Sign In</Link>
+          <div className="content-display">
+            <Route path="/signup" component={SignUp} />
+            <Route path="/signin" component={SignIn} />
+            <Route path="/" exact={true} component={Home} />
+          </div>
         </div>
-      </BrowserRouter>  
+      </div>
+    </BrowserRouter>
   );
 }
 
--- agora-app/front-end/src/screens/SignIn.js
+++ agora-app/front-end/src/screens/SignIn.js
@@ -1,19 +1,12 @@
-<<<<<<< HEAD
 import React from "react";
-import ReactDOM from "react-dom";
-import "./App.css";
-import App from "./App";
-=======
-import React from 'react';
 
-function SignIn (props) {
-    return <div className="sign-in-container">
-        This is the sign in page
-        <div>
-            A sign in form goes here
-        </div>
+function SignIn(props) {
+  return (
+    <div className="sign-in-container">
+      This is the sign in page
+      <div>A sign in form goes here</div>
     </div>
+  );
 }
 
 export default SignIn;
->>>>>>> c24dcb02ca5e143559ca76f7a8dde846c2072c9c
