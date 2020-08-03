bdab27d7de4f39a442fbe874dee6f1cc9404b3d1
stece850 <stece850@student.otago.ac.nz>
2020-08-03 19:19:57 +1200

    updating to get latest release

--- agora-app/front-end/src/screens/SignUp.js
+++ agora-app/front-end/src/screens/SignUp.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
-function SignUp (props) {
-    return <div className="sign-up-container">
-        Hello CeDriC! Is this displaying ?
-    </div>
+function SignUp(props) {
+  return (
+    <div className="sign-up-container">Hello Cedric! Is this displaying ?</div>
+  );
 }
 
 export default SignUp;
