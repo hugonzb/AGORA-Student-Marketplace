ef67dd4ae36ed032701604bffa8061f32876491d
Hugo Baird <hugonzb@gmail.com>
2020-08-03 18:49:02 +1200

    Created sign up screen default rendering

--- agora-app/front-end/src/App.js
+++ agora-app/front-end/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
+import Home from './screens/Home';
+import SignUp from './screens/SignUp';
 
 function App() {
   return (
       <BrowserRouter>
         <div className="container">
           <div className="main">
-                <div className="content-display">
-                    <Route path="/signup" component={SignUp} />
-                    <Route path="/" exact={true} component={Home} />
-                </div>
+              <Link to="/signup">Sign Up</Link>
+              <div className="content-display">
+                  <Route path="/signup" component={SignUp} />
+                  <Route path="/" exact={true} component={Home} />
+              </div>
             </div>
         </div>
     </BrowserRouter>  
---
+++ agora-app/front-end/src/screens/SignUp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function SignUp (props) {
+    return <div className="sign-up-container">
+        Hello CeDriC! Is this displaying ?
+    </div>
+}
+
+export default SignUp;
