42d297221ce231a641364ec2c2413039e84b3df2
Leon <le.o.n@outlook.com>
2020-08-04 12:01:53 +1200

    Fixed up package.json

--- agora-app/back-end/server.js
+++ agora-app/back-end/server.js
@@ -8,4 +8,4 @@ app.get("/api/listings", (req, res) => {
 });
 
 /* run server on port 5000 */
-app.listen(5000, () => {console.log("Server started at http://localhost:5000")});
+app.listen(5000, () => {console.log("Server started at http://localhost:5000\nListings are on page: http://localhost:5000/api/listings")});
