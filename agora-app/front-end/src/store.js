8b1a0be5b9c1a992cca1f00bb534fa2bc357c08c
Hugo Baird <hugonzb@gmail.com>
2020-08-04 13:34:27 +1200

    Added redux components

--- agora-app/front-end/src/store.js
+++ agora-app/front-end/src/store.js
@@ -1,1 +1,12 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { listingReducer } from './reducers/listingReducer';
+import { thunk } from 'redux-thunk';
+
+const initialState = {};
+const reducer = combineReducers({
+    listings: listingReducer,
+})
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
 export default store;
