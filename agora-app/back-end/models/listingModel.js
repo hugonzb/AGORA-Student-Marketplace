acc8d3b5e6bf8638e58143f1ddbba6cd3de4dfaa
Hugo Baird <hugonzb@gmail.com>
2020-08-09 17:09:16 +1200

    Added more listing fields to database model

--- agora-app/back-end/models/listingModel.js
+++ agora-app/back-end/models/listingModel.js
@@ -3,8 +3,11 @@ import mongoose from 'mongoose';
 // Database schema for a listing
 const listingSchema = new mongoose.Schema({
     name:{ type: String, required: true },
+    description:{ type: String, required: true },
     category:{ type: String, required: true },
     price:{ type: Number, required: true },
+    location:{ type: String, required: true },
+    university:{ type: String, required: true },
     brand:{ type: String, required: false },
     seller:{ type: String, required: true }
 });
