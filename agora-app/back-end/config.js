91910339ba7f4d1da6e14e0ef29b32213905e7c9
Hugo Baird <hugonzb@gmail.com>
2020-08-09 13:59:33 +1200

    Added mongodb url to database config files

--- agora-app/back-end/config.js
+++ agora-app/back-end/config.js
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-    // change the url to the public mongodb url when connecting to the db
-    MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/agora',
+    MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://agora_admin:agora123@cluster0.xbemo.mongodb.net/agora?retryWrites=true&w=majority',
     JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret'
 }
