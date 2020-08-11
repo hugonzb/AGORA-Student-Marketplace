aeb30a4223b4d490eb527a942d87a518bdf956e6
stece850 <stece850@student.otago.ac.nz>
2020-08-11 12:38:09 +1200

    stashing local changes

--- agora-app/back-end/config.js
+++ agora-app/back-end/config.js
@@ -1,20 +1,10 @@
-<<<<<<< HEAD
 import dotenv from "dotenv";
-=======
-import dotenv from 'dotenv';
->>>>>>> 1fa32b73359c027ca8ed650e40b6c033e0f5e5ae
 
 dotenv.config();
 
 export default {
-<<<<<<< HEAD
-  PORT: process.env.PORT || 5000,
   MONGODB_URL:
     process.env.MONGODB_URL ||
-    "mongodb+srv://agora_admin:agora123@cluster0.xbemo.mongodb.net/AgoraMarketplace?retryWrites=true&w=majority",
+    "mongodb+srv://agora_admin:agora123@cluster0.xbemo.mongodb.net/agora?retryWrites=true&w=majority",
+  JWT_SECRET: process.env.JWT_SECRET || "somethingsecret",
 };
-=======
-    MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://agora_admin:agora123@cluster0.xbemo.mongodb.net/agora?retryWrites=true&w=majority',
-    JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret'
-}
->>>>>>> 1fa32b73359c027ca8ed650e40b6c033e0f5e5ae
