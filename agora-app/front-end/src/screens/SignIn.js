bd64d04730dc924d83c50a37f48911bd820ab18f
Leon <le.o.n@outlook.com>
2020-08-13 16:06:29 +1200

    start working on SignIn.js form

--- agora-app/front-end/src/screens/SignIn.js
+++ agora-app/front-end/src/screens/SignIn.js
@@ -4,7 +4,27 @@ function SignIn(props) {
   return (
     <div className="sign-in-container">
       This is the sign in page
-      <div>A sign in form goes here</div>
+      <div className="signInContainer">
+        <form className="sign-in-form" onSubmit={submitHandler}>
+          <label for="uname">Username:</label>
+          <input
+          type="text"
+          id="uname"
+          name="uname"
+          placeholder="Username"
+          onChange={(e) => setUname(e.target.value)}
+          ></input>
+          <br></br>
+          <label for="password">Password:</label>
+          <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+          ></input>
+        </form>
+      </div>
     </div>
   );
 }
