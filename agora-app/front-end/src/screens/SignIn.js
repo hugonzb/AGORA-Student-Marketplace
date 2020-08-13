95ca163b7dc793a34600cd8637c305c35d008295
Leon <le.o.n@outlook.com>
2020-08-13 16:32:49 +1200

    Have done most of SignIn.js form, I guess I'll start on some of the reducer/action/constants stuff

--- agora-app/front-end/src/screens/SignIn.js
+++ agora-app/front-end/src/screens/SignIn.js
@@ -1,27 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 function SignIn(props) {
+  const [uname, setUname] = useState("");
+  const [password, setPassword] = useState("");
+  //const {loading, userInfo, error} = userSignin;
+
+  const dispatch = useDispatch();
+
+  /* This might need to be updated to match
+  the useEffect() from the video */
+  /*useEffect(() => {
+    return () => { };
+  }, [userInfo])
+*/
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(
+      SignIn(
+        uname, password
+      )
+    );
+  };
+
   return (
     <div className="sign-in-container">
       This is the sign in page
-      <div className="signInContainer">
-        <form className="sign-in-form" onSubmit={submitHandler}>
+      <div className="createAccountContainer">
+        <form className="create-account-form" onSubmit={submitHandler}>
           <label for="uname">Username:</label>
           <input
-          type="text"
-          id="uname"
-          name="uname"
-          placeholder="Username"
-          onChange={(e) => setUname(e.target.value)}
+            type="text"
+            id="uname"
+            name="uname"
+            placeholder="Username"
+            onChange={(e) => setUname(e.target.value)}
           ></input>
           <br></br>
           <label for="password">Password:</label>
           <input
-          type="password"
-          id="password"
-          name="password"
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
           ></input>
         </form>
       </div>
