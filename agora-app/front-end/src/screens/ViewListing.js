0f088def55bfde1c34817a2724ec761cb55e2065
Hugo Baird <hugonzb@gmail.com>
2020-08-16 19:29:08 +1200

    Listing container styling

--- agora-app/front-end/src/screens/ViewListing.js
+++ agora-app/front-end/src/screens/ViewListing.js
@@ -2,11 +2,11 @@ import React from "react";
 
 function ViewListing(props) {
 	return (
-	<div className="listing-container">
-	    <div classname="listing-details">
-			Listing is here.	
+		<div className="listing-container">
+			<div classname="listing-details">
+				Listing is here.	
+			</div>
 		</div>
-	</div>
 	);
 }
 
