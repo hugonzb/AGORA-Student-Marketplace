99bf5c0f10345f2dd328a45bf3d36d0f9a77e128
Hugo Baird <hugonzb@gmail.com>
2020-08-16 19:01:07 +1200

    Base layout of view listing screen

--- agora-app/front-end/src/screens/ViewListing.js
+++ agora-app/front-end/src/screens/ViewListing.js
@@ -3,20 +3,9 @@ import React from "react";
 function ViewListing(props) {
 	return (
 	<div className="listing-container">
-	Your Profile
-		<div className="createAccountContainer">
-			<form className="profile-container">
-			<h2> Profile </h2>
-			<label for="username">Student ID:</label>
-			<br></br>
-			<label for="name">Name:</label>
-			<br></br>
-			</form>
-			</div>
-
-
-
-	
+	    <div classname="listing-details">
+			Listing is here.	
+		</div>
 	</div>
 	);
 }
