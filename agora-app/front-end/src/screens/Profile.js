a5281fff5d934b2dee39cfc08ade155a39802df5
Leon <le.o.n@outlook.com>
2020-08-18 10:12:36 +1200

    Changed Profile.js to import logout from userActions as a non-default export of userActions.

--- agora-app/front-end/src/screens/Profile.js
+++ agora-app/front-end/src/screens/Profile.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import useDispatch, { useSelector } from 'react-redux';
-import logout from '../actions/userActions';
+import {logout} from '../actions/userActions';
 
 function Profile(props) {
 
