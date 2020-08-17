a8fbab207a09b6e3c2b659aa90eb975cb5d9aba1
Leon <le.o.n@outlook.com>
2020-08-18 10:15:39 +1200

    Made useDispatch a non-default import from react-redux in Profile.js

--- agora-app/front-end/src/screens/Profile.js
+++ agora-app/front-end/src/screens/Profile.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import useDispatch, { useSelector } from 'react-redux';
+import {useDispatch, useSelector } from 'react-redux';
 import {logout} from '../actions/userActions';
 
 function Profile(props) {
