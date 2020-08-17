3405a48b9038af60a35c744aea62485b787714a9
Leon <le.o.n@outlook.com>
2020-08-18 10:08:34 +1200

    Trying to add id and name fields to the Profile.js screen.

--- agora-app/front-end/src/screens/Profile.js
+++ agora-app/front-end/src/screens/Profile.js
@@ -6,10 +6,11 @@ function Profile(props) {
 
 	const [name, setName] = useState('');
 	const [email, setEmail] = useState('');
+	const [id, setID] = useState('');
 
 	const userSignin = useSelector(state => state.userSignin);
-	const {userInfo} = userSignin;
-	
+	const { userInfo } = userSignin;
+
 	const dispatch = useDispatch();
 
 
@@ -20,34 +21,35 @@ function Profile(props) {
 		props.history.push("/signin");
 	}
 
-	useEffect(()=> {
+	useEffect(() => {
 		// If userInfo exists, set the email and name fields
-		if(userInfo) {
+		if (userInfo) {
 			// Log the name field just to make sure it's correct
 			console.log(userInfo.name);
 			setEmail(userInfo.email);
 			setName(userInfo.name);
+			setName(userInfo.id);
 		}
-		return() => {};
+		return () => { };
 	}, [userInfo])
 
 	return (
-	<div className="sign-in-container">
-	Your Profile
-		<div className="createAccountContainer">
-			<form className="profile-container">
-			<h2> Profile </h2>
-			<label for="username">Student ID:</label>
-			<br></br>
-			<label for="name">Name:</label>
-			<br></br>
-			</form>
+		<div className="sign-in-container">
+			Your Profile
+			<div className="createAccountContainer">
+				<form className="profile-container">
+					<h2> Profile </h2>
+					<label for="username">Student ID: {{ id }}</label>
+					<br></br>
+					<label for="name">Name:{{ name }}</label>
+					<br></br>
+				</form>
 			</div>
 
 
 
-	
-	</div>
+
+		</div>
 	);
 }
 
