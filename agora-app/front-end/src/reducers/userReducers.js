043039fafbbe3a861e5b338700fc56df2e8f3d4e
Hugo Baird <hugonzb@gmail.com>
2020-08-18 12:11:50 +1200

    Remove duplicate import in user reducer

--- agora-app/front-end/src/reducers/userReducers.js
+++ agora-app/front-end/src/reducers/userReducers.js
@@ -1,7 +1,4 @@
-import {USER_SIGNUP_REQUEST, USER_SIGNUP_SUCCESS, USER_SIGNUP_FAIL} from '../constants/userConstants';
-import {USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_SIGNIN_FAIL} from '../constants/userConstants';
-
-
+import {USER_SIGNUP_REQUEST, USER_SIGNUP_SUCCESS, USER_SIGNUP_FAIL, USER_LOGOUT} from '../constants/userConstants';
 
 function userSignupReducer(state={}, action){
     switch(action.type){
