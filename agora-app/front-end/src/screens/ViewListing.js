7fac173ab7cb2b2b5e846aa6507f2b1458a64e26
Hugo Baird <hugonzb@gmail.com>
2020-08-18 12:33:08 +1200

    Created div container elements for view listing layout

--- agora-app/front-end/src/screens/ViewListing.js
+++ agora-app/front-end/src/screens/ViewListing.js
@@ -3,8 +3,19 @@ import React from "react";
 function ViewListing(props) {
 	return (
 		<div className="listing-container">
+			<div classname="listing-top">
+				<div classname="listing-image">
+					Listing image is here.	
+				</div>	
+				<div classname="listing-actions">
+					Listing actions are here.	
+				</div>	
+			</div>
 			<div classname="listing-details">
-				Listing is here.	
+				Listing information is here.	
+			</div>
+			<div classname="listing-qa-container">
+				Listing questions and answers.	
 			</div>
 		</div>
 	);
