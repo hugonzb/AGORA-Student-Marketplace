ba2d49932cf66eb82ff296a5df83c4327797977e
stece850 <stece850@student.otago.ac.nz>
2020-08-18 12:45:53 +1200

    fixed bug which said it could not find contact screen in app imports

--- agora-app/front-end/src/App.js
+++ agora-app/front-end/src/App.js
@@ -1,23 +1,22 @@
 import React from "react";
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import Home from "./screens/Home";
 import SignUp from "./screens/SignUp";
 import SignIn from "./screens/SignIn";
 import About from "./screens/About";
-import contact from "./screens/Contact";
+import contact from "./screens/contact";
 import ViewListing from "./screens/ViewListing";
 import Profile from "./screens/Profile";
 import agoralogo from "./images/agoralogo.png";
 import profileicon from "./images/profileicon.png";
-import Navbar from './components/navbar.jsx';
-
+import Navbar from "./components/navbar.jsx";
 
 function App() {
   const dispatch = useDispatch();
-  const userSignin = useSelector(state=>state.userSignin);
-  const {userInfo} = userSignin;
-  
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+
   return (
     <BrowserRouter>
       <div className="container">
@@ -25,44 +24,54 @@ function App() {
           <Link to="/">
             <div className="header-logo">
               <img className="agora-logo" src={agoralogo} alt="agoralogo"></img>
-              <div className="agora-text">AGORA </div> <div className="sm-text"> Student Marketplace</div>
+              <div className="agora-text">AGORA </div>{" "}
+              <div className="sm-text"> Student Marketplace</div>
             </div>
-          </Link>  
+          </Link>
           <nav className="links">
             <Link to="/">HOME </Link>
-            { userInfo ? ( <Link to="/profile" >Welcome {userInfo.fname}</Link>)
-                             :
-                                (   <div>
-                                    <Link to="/signup">REGISTER </Link>
-                                    <Link to="/signin">SIGN IN </Link></div> )
-            }
+            {userInfo ? (
+              <Link to="/profile">Welcome {userInfo.fname}</Link>
+            ) : (
+              <div>
+                <Link to="/signup">REGISTER </Link>
+                <Link to="/signin">SIGN IN </Link>
+              </div>
+            )}
             <Link to="/About">ABOUT </Link>
             <Link to="/contact">CONTACT US </Link>
-            <Link to="/profile"><img className="profile-icon" src={profileicon} alt="profile"></img>
+            <Link to="/profile">
+              <img
+                className="profile-icon"
+                src={profileicon}
+                alt="profile"
+              ></img>
             </Link>
           </nav>
         </div>
         <div className="search">
-          <div> 
+          <div>
             <form>
-              <input className = "search-input" name="searchWord" placeholder="What are you looking for? .."/>
+              <input
+                className="search-input"
+                name="searchWord"
+                placeholder="What are you looking for? .."
+              />
             </form>
           </div>
         </div>
         <div className="main">
           <div className="content-display">
-            <Route path="/signup" component={SignUp}/>
-            <Route path="/signin" component={SignIn}/>   
-            <Route path="/About" component={About}/>
-            <Route path="/contact" component={contact}/>          
-            <Route path="/profile" component={Profile}/>          
-            <Route path="/" exact={true} component={Home}/>
+            <Route path="/signup" component={SignUp} />
+            <Route path="/signin" component={SignIn} />
+            <Route path="/About" component={About} />
+            <Route path="/contact" component={contact} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/" exact={true} component={Home} />
             <Route path="/listing/:id" component={ViewListing} />
           </div>
         </div>
-        <div className="footer">
-          All Rights Reserved.
-        </div>
+        <div className="footer">All Rights Reserved.</div>
       </div>
     </BrowserRouter>
   );
