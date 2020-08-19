897fd24977055fea3f8d675f53ae86e8457c273f
Hugo Baird <hugonzb@gmail.com>
2020-08-19 16:38:14 +1200

    Flex containers for responsiveness in view listing

--- agora-app/front-end/src/screens/ViewListing.js
+++ agora-app/front-end/src/screens/ViewListing.js
@@ -3,18 +3,16 @@ import React from "react";
 function ViewListing(props) {
 	return (
 		<div className="view-listing-container">
-			<div className="view-listing-top"> 
 				<div className="view-listing-image">
 					Listing image is here.	
-				</div>	
-				<div className="listing-actions">
+				</div>	 
+				<div className="view-listing-actions">
 					Listing actions are here.	
 				</div>	
-			</div>
-			<div className="listing-details">
+			<div className="view-listing-details">
 				Listing information is here.	
 			</div>
-			<div className="listing-qa-container">
+			<div className="view-listing-qa-container">
 				Listing questions and answers.	
 			</div>
 		</div>
