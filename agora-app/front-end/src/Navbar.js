69c9ff42b03e1758195351e1a54ee5e92f5a71c7
Vainui Moresi <zedricvm@gmail.com>
2020-08-20 21:04:15 +1200

    Formatting css to align elements correctly on screen.

--- agora-app/front-end/src/Navbar.js
+++ agora-app/front-end/src/Navbar.js
@@ -7,15 +7,15 @@ class Navbar extends Component{
 render(){
 	return(
 		<nav> 
-      <div className="btnNav">
+      <div className="logoBtn">
       <img className="agora-logo" src={agoralogo} alt="agoralogo"></img>
               <div className="agora-text">AGORA </div>{" "}
               <div className="sm-text"> Student Marketplace</div>
 
-                    <div className="btnNav">
-                        <div className="barNav"></div>
-                        <div className="barNav"></div>
-                        <div className="barNav"></div>
+                    <div className="btn">
+                        <div className="bar"></div>
+                        <div className="bar"></div>
+                        <div className="bar"></div>
                     
                     </div>
                 </div>
