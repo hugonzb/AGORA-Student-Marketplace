2233b8f4d916ec32ccef7a1f4c272a2145d3117e
husam <sam-788@hotmail.com>
2020-08-23 12:42:55 +1200

    style contact us page

--- agora-app/front-end/src/screens/contact.js
+++ agora-app/front-end/src/screens/contact.js
@@ -2,49 +2,26 @@ import React from "react";
 
 function Contact(props) {
   return (
-    <div className="container-contact">
-      <form className="form-contact">
-        <fieldset className="contact-field">
-          <label for="fname">Full Name</label>
-          <input
-            type="text"
-            id="fname"
-            name="firstname"
-            placeholder="Your first name"
-            required
-          />
-
-          <label for="email">Email</label>
-          <input type="text" placeholder="Enter Email" name="email" required />
-
-          <label for="country">Region</label>
-          <input
-            type="text"
-            id="Rname"
-            name="Region"
-            placeholder="Your Region"
-            required
-          />
-
-          <label for="country">University</label>
-          <input
-            type="text"
-            id="Uname"
-            name="University"
-            placeholder="Your University"
-          />
-
-          <label for="subject">Subject</label>
-          <textarea
-            id="subject"
-            name="subject"
-            placeholder="write massege here"
-          ></textarea>
-
-          <input type="submit" value="Submit" />
-        </fieldset>
-      </form>
-    </div>
+    <div className="c-wrapper">
+      <div className="c-container">
+        <div className="c-contactus">
+          <h1>contact us</h1>
+          <div className="c-item name_email">
+            <input type="text" placeholder="Your name"/>
+              <input type="text" placeholder="Email"/>		
+      </div>
+              <div className="c-item">
+                <input type="text" placeholder="Subject"/>		
+      </div>
+                <div className="c-item">
+                  <textarea placeholder="Message"></textarea>
+                </div>
+                <div className="c-btn">
+                  <a href="#">send</a>
+                </div>
+              </div>
+  </div>
+          </div>	
   );
 }
 
