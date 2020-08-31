d32fbbe4ab9b1feb3ea1d54c08947f89d4fe3065
Hugo Baird <hugonzb@gmail.com>
2020-08-31 15:58:13 +1200

    indented and cleaned navbar file

--- agora-app/front-end/src/Navbar.js
+++ agora-app/front-end/src/Navbar.js
@@ -1,52 +1,45 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import agoralogo from "./images/agoralogo.png";
 import "./navbar.css";
-import { BrowserRouter, Route, Link } from "react-router-dom";
-import Home from "./screens/Home";
-import SignIn from "./screens/SignIn";
+import { Link } from "react-router-dom";
+
 class Navbar extends Component{
+    state = {
+        isOpen:false
+    }
 
-state={
-    isOpen:false
-}
+    handleClick=()=>{
+        this.setState({
+        isOpen:!this.state.isOpen,
+        })
+    }
 
-handleClick=()=>{
-    this.setState({
-     isOpen:!this.state.isOpen,
-	})
-}
-
-render(){
-	return(
-	
-    <nav>
-      <div className="logoBtn">
-            <div className="logo">
-            <a href="/"><img src={agoralogo} 
-            className="logo" alt=""/></a> 
-            </div>
-
+    render(){
+        return(  
+            <nav>
+                <div className="logoBtn">
+                    <div className="logo">
+                        <Link to="/">
+                            <img src={agoralogo} className="logo" alt=""/>
+                        </Link> 
+                    </div>
                     <div className="btn" onClick=
-                    {this.handleClick}>
+                        {this.handleClick}>
                         <div className="bar"></div>
                         <div className="bar"></div>
                         <div className="bar"></div>
                     </div>          
-             </div>
-
-              <ul className={this.state.isOpen ? 
-              "showNav":"undefined"}>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/signup">Register</Link></li>
-              <li><Link to="/signin">Sign In</Link></li>
-              <li><Link to="/About">About</Link></li>
-              <li><Link to="/Contact">Contact Us</Link></li> 
-              </ul>
-         </nav>
-               
-	)
-}
-
+                </div>
+                <ul className={this.state.isOpen ? "showNav":"undefined"}>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/signup">Register</Link></li>
+                    <li><Link to="/signin">Sign In</Link></li>
+                    <li><Link to="/About">About</Link></li>
+                    <li><Link to="/Contact">Contact Us</Link></li> 
+                </ul>
+            </nav>          
+        )
+    }
 }
 
 export default Navbar;
