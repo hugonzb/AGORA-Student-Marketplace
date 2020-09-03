dcb7b9872769ab31e7526e6596535edb8fac184b
Hugo Baird <hugonzb@gmail.com>
2020-09-03 16:25:15 +1200

    Added filter boxes (not working currently)

--- agora-app/front-end/src/screens/Home.js
+++ agora-app/front-end/src/screens/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from "react-router-dom";
 import { listListings } from '../actions/listingActions';
@@ -6,6 +6,8 @@ import profileicon from "../images/profileicon.png";
 
 
 function Home (props) {
+    const [categorySortOrder, setCategorySortOrder] = useState('');
+    const [locationSortOrder, setLocationSortOrder] = useState('');
     const listingList = useSelector(state => state.listingList);
     const { listings, loading, error } = listingList;
     const dispatch = useDispatch();
@@ -17,9 +19,31 @@ function Home (props) {
         // eslint-disable-next-line
     }, [])
 
+    const submitHandler = (e) =>{
+        e.preventDefault();
+    }
+
     return loading ? <div className="loading">Loading listings ...</div> : 
-    error? <div className="error"> {error} - Make sure you are running the server to fetch data ;) </div> :
+    error? <div className="error"> {error} - Make sure you are running the server to fetch data ;)</div> :
     <div className="home-container">
+            <form onSubmit={submitHandler}>
+                <select name="categorySortOrder" className="select-style" onChange={(e) => {setCategorySortOrder(e.target.value)}}>
+                    <option value=" "> All Categories </option>
+                    <option value="healthandfitness"> Health & Fitness </option>
+                    <option value="option2"> option 2 </option>
+                    <option value="option3"> option 3 </option>
+                </select>
+                <button className = "filter-button" type = "submit"></button>
+            </form>
+            <form onSubmit={submitHandler}>
+                <select name="locationSortOrder" className="select-style" onChange={(e) => {setLocationSortOrder(e.target.value)}}>
+                    <option value=" "> All Locations </option>
+                    <option value="option1"> option1 </option>
+                    <option value="option2"> option2 </option>
+                    <option value="option3"> option3 </option>
+                </select>
+                <button className = "filter-button" type = "submit"></button>
+            </form>
         <div className="listings">
             {listings.map(listing =>
             <Link to={'/listing/' + listing._id}>
