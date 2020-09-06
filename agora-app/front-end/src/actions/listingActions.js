1ca1da8d29594193381537422465ef1cd499b546
Hugo Baird <hugonzb@gmail.com>
2020-09-06 15:57:23 +1200

    Fixed merge conflicts

--- agora-app/front-end/src/actions/listingActions.js
+++ agora-app/front-end/src/actions/listingActions.js
@@ -32,7 +32,7 @@ const detailListing = (listingId) => async (dispatch) => {
 }
 
 const createListing = (name, description, image, category, price, location,
-    university, brand, seller) => {
+    university, brand, seller) => async (dispatch) => {
             dispatch({type: CREATELISTING_REQUEST, payload: {
                 name, description, image, category, price, location,
                 university, brand, seller}});
