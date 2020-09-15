b1e30b66d458584e98ffba500884a6b848a26009
Leon <le.o.n@outlook.com>
2020-09-15 13:53:27 +1200

    Have started working on facilitating uploading an image. Currently backend code is written up to support uploading a file at /api/listings/uploadimage

--- agora-app/back-end/routes/listingRoute.js
+++ agora-app/back-end/routes/listingRoute.js
@@ -1,5 +1,7 @@
 import express from "express";
+import multer from "multer";
 import Listing from "../models/listingModel";
+import { createBrotliCompress } from "zlib";
 
 const router = express.Router();
 
@@ -72,6 +74,22 @@ router.get("/account/profile"),
       res.status(404).send({ message: "User Listings not found." });
     }
   };
+
+// Method which will give us the filename and path of an uploaded image
+const storage = multer.diskStorage({
+  destination(req, file, cb){
+    cb(null, 'uploads/')
+  },
+  filename(req, file){
+    cb(null, Date.now() + '.jpg')
+  }
+});
+
+const upload = multer({storage});
+
+router.post("/uploadimage", upload.single('image'), (req, res) => {
+  res.send('/' + req.file.path);
+});
 
 // Hope it's ok to make the post uri to /listing/create
 router.post("/create", async (req, res) => {
