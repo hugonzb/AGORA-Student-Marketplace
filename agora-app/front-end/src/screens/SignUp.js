6e59b710f438c5fe05865959c1c4f9ad5b3ea6a7
Vainui Moresi <zedricvm@gmail.com>
2020-09-17 23:04:46 +1200

    Reverted back to original dropdown menu functions,.

--- agora-app/front-end/src/screens/SignUp.js
+++ agora-app/front-end/src/screens/SignUp.js
@@ -5,9 +5,8 @@ import { Link } from "react-router-dom";
 import "../signup.css";
 import { removeUser } from "../actions/userActions";
 import agoralogo from "../images/agoralogo.png";
-import Select from "react-select";
 import Axios from "axios";
-import makeAnimated from "react-select/animated";
+
 
 function SignUp(props) {
   const [fname, setFname] = useState("");
@@ -84,184 +83,155 @@ function SignUp(props) {
       )
     );
   };
-
-  const animatedComponents = makeAnimated();
-
-  const Gender = [
-
-    { label: "Male", value: "Male" },
-    { label: "Female", value: "Female" },
-    { label: "Other", value: "Other" },
-
-  ];
-
-  const Universities = [
-    { label: "University of Auckland", value: "University of Auckland" },
-    { label: "Auckland University of Technology", value: "Auckland University of Technology" },
-    { label: "University of Waikato", value: "University of Waikato" },
-    { label: "Massey University", value: "Massey University" },
-    { label: "Victoria University of Wellington", value: "Victoria University of Wellington" },
-    { label: "University of Canterbury", value: "University of Canterbury" },
-    { label: "Lincoln University", value: "Lincoln University" },
-    { label: "University of Otago", value: "University of Otago" },
-  ];
-
+ 
   return (
     <div className="wrapper">
-      <div className="registration_form">
-        <div className="form-heading">
-          <img src={agoralogo} className="sign-up-logo" alt="" />
-
-          <span>
-            <h4>Registration Form</h4>
-            Already have an account?
-            <h4>
-              <Link to="/account/signin">Sign in</Link>
-            </h4>
-          </span>
-        </div>
-        {loading}
+        <div className="registration_form">
+            <div className="form-heading">
+                
+               
+                   <img src={agoralogo} 
+                   className="sign-up-logo" alt=""/>
+               
+                   <span> 
+			       <h4>Registration Form</h4> 
+                   Already have an account?  
+                   <h4><Link to="/account/signin">Sign in</Link></h4>
+                   </span>
+             
+                
+            </div>
+            {loading}
         {error && <div>Student ID or Email Address has been taken.</div>}
         <form className="form_wrap" onSubmit={submitHandler}>
-          <div className="input_grp">
-            <div className="input_wrap">
-              <label>First Name:</label>
-              <input
-                type="text"
-                id="fname"
-                name="fname"
-                placeholder="Enter First Name"
-                required
-                onChange={(e) => setFname(e.target.value)}
-              ></input>
-            </div>
-            <div className="input_wrap">
-              <label>Last Name:</label>
-              <input
-                type="text"
-                id="sname"
-                name="sname"
-                placeholder="Enter Surname"
-                required
-                onChange={(e) => setSname(e.target.value)}
-              ></input>
-            </div>
-          </div>
-
-          <div className="input_grp">
-            <div className="input_wrap">
-              <label>Student ID:</label>
-              <input
-                type="text"
-                id="studentid"
-                name="studentid"
-                placeholder="Enter Student-ID"
-                required
-                onChange={(e) => setStudentid(e.target.value)}
-              ></input>
-            </div>
-
-            <div className="input_wrap">
-              <label>Email:</label>
-              <input
-                type="text"
-                id="email"
-                name="email"
-                placeholder="Enter Email"
-                required
-                onChange={(e) => setEmail(e.target.value)}
-              ></input>
-            </div>
-          </div>
-
-          <div className="input_grp">
-            <div className="input_wrap">
-              <label>Username:</label>
-              <input
-                type="text"
-                id="username"
-                name="username"
-                placeholder="Enter a Username"
-                required
-                onChange={(e) => setUsername(e.target.value)}
-              ></input>
-            </div>
-
-            <div className="input_wrap">
-              <label>Password:</label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                placeholder="Enter Password"
-                required
-                onChange={(e) => setPassword(e.target.value)}
-              ></input>
-            </div>
-
-          </div>
-
-          <div className="input_wrap">
-            <label>Upload a profile picture</label>
-            <input type="file" onChange={uploadFileHandler}></input>
-            {upLoading && <div>Uploading...</div>}
-          </div>
-
-          <div className="input_grp">
-            <div className="input_wrap">
-              <label>Username:</label>
-              <input
-                type="text"
-                id="username"
-                name="username"
-                placeholder="Enter a Username"
-                required
-                onChange={(e) => setUsername(e.target.value)}
-              ></input>
-            </div>
-
-            <div className="input_wrap">
-              <label>Password:</label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                placeholder="Enter Password"
-                required
-                onChange={(e) => setPassword(e.target.value)}
-              ></input>
-            </div>
-          </div>
-
-          <div className="input_grp">
-            <div className="input_wrap">
-              <div className="gender">
-                <div className="container">
-                  <div className="row">
-                    <div className="col-md-3"></div>
-                    <div className="col-md-6">
-                      <Select options={Gender} components={animatedComponents} onChange={(e) => setGender(e.target.value)} />
-                    </div>
-                    <div classNane="col-md-4"></div>
-                  </div>
+            <div className="input_grp">
+          
+                <div className="input_wrap"> 
+                   <label>First Name:</label>
+                   <input
+                   type="text"
+                        id="fname"
+                        name="fname"
+                        placeholder="Enter First Name"
+                        required
+                        onChange={(e) => setFname(e.target.value)}
+                      ></input>
+                </div>
+                <div className="input_wrap">
+                      <label>Last Name:</label>
+                      <input
+                        type="text"
+                        id="sname"
+                        name="sname"
+                        placeholder="Enter Surname"
+                        required
+                        onChange={(e) => setSname(e.target.value)}
+                      ></input>
                 </div>
               </div>
-            </div>
-
-            <div className="input_wrap">
-              <label for="DOB"> Date of Birth:</label>
-              <input
-                type="date"
-                id="DOB"
-                name="DOB"
-                required
-                onChange={(e) => setDOB(e.target.value)}
-              ></input>
-            </div>
+ 
+            <div className="input_grp">
+             <div className="input_wrap">
+                  <label>Student ID:</label>
+                  <input 
+                    type="text"
+                    id="studentid"
+                    name="studentid"
+                    placeholder="Enter Student-ID"
+                    required
+                    onChange={(e) => setStudentid(e.target.value)}
+                    ></input>
+                </div>
+          
+              <div className="input_wrap">
+                  <label>Email:</label>
+                  <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    placeholder="Enter Email"
+                    required
+                    onChange={(e) => setEmail(e.target.value)}
+                  ></input>
+               </div>
           </div>
-
+    
           <div className="input_grp">
-            <div className="input_wrap">
+              <div className="input_wrap">
+                  <label>Username:</label>
+                  <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    placeholder="Enter a Username"
+                    required
+                    onChange={(e) => setUsername(e.target.value)}
+                  ></input>
+              </div>
+ 
+              <div className="input_wrap">
+                  <label>Password:</label>
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="Enter Password"
+                    required
+                    onChange={(e) => setPassword(e.target.value)}
+                  ></input>
+              </div>
+ 
+          </div> 
+          
+         <div className="input_grp">
+              <div className="input_wrap">
+                  <label for="gender">Choose a Gender: </label>
+                  <select
+                    id="gender"
+                    name="gender"
+                    onChange={(e) => setGender(e.target.value)}
+                  >
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                    <option value="Other">Other</option>
+                  </select>
+              </div>
+
+                   <div className="input_wrap">
+              <label for="school">University: </label>
+              <select
+                id="school"
+                name="school"
+                placeholder="Select University"
+                onChange={(e) => setUniversity(e.target.value)}
+              >
+                <option value="University of Auckland">
+                  University of Auckland
+                </option>
+                <option value="Auckland University of Technology (AUT)">
+                  Auckland University of Technology (AUT)
+                </option>
+                <option value="University of Waikato">University of Waikato</option>
+                <option value="Massey University">Massey University</option>
+                <option value="Victoria University of Wellington">
+                  Victoria University of Wellington
+                </option>
+                <option value="University of Canterbury">
+                  University of Canterbury
+                </option>
+                <option value="Lincoln University">Lincoln University</option>
+                <option value="University of Otago">University of Otago</option>
+          </select>
+ 
+              </div>
+              </div>
+ 
+          
+ 
+         
+ 
+          <div className="input_grp">
+               <div className="input_wrap">
               <label>Address:</label>
               <input
                 type="text"
@@ -271,9 +241,9 @@ function SignUp(props) {
                 required
                 onChange={(e) => setStreet(e.target.value)}
               ></input>
-            </div>
-
-            <div className="input_wrap">
+          </div> 
+ 
+          <div className="input_wrap">
               <label>City:</label>
               <input
                 type="text"
@@ -283,11 +253,11 @@ function SignUp(props) {
                 required
                 onChange={(e) => setCity(e.target.value)}
               ></input>
-            </div>
+          </div> 
           </div>
-
+          
           <div className="input_grp">
-            <div className="input_wrap">
+              <div className="input_wrap">
               <label>Post Code:</label>
               <input
                 type="text"
@@ -296,39 +266,45 @@ function SignUp(props) {
                 placeholder="Enter Postcode"
                 required
                 onChange={(e) => setPostcode(e.target.value)}
-              ></input>
-            </div>
+               ></input>
+              </div> 
 
-            <div className="input_wrap">
-              <div className="universities">
-                <div className="container">
-                  <div className="row">
-                    <div className="col-md-3"></div>
-                    <div className="col-md-6">
-                      <Select options={Universities} components={animatedComponents} onChange={(e) => setUniversity(e.target.value)} />
-                    </div>
-                    <div classNane="col-md-4"></div>
-                  </div>
-                </div>
+                  <div className="input_wrap">
+                  <label for="DOB"> Date of Birth:</label>
+                  <input
+                    type="date"
+                    id="DOB"
+                    name="DOB"
+                    required
+                    onChange={(e) => setDOB(e.target.value)}
+                  ></input>
               </div>
-            </div>
-
+          </div>
+          
+        <div className="input_grp">
+           <div className="input_wrap">
+            <label>Upload a profile picture</label>
+            <input type="file" onChange={uploadFileHandler}></input>
+            {upLoading && <div>Uploading...</div>}
+          </div>
           </div>
 
-
+ <div className="input_grp">
+ 
           <div class="input_wrap">
-            <input type="submit" value="Register Now" class="submit_btn"></input>
-          </div>
-
+					<input type="submit" value="Register Now" class="submit_btn"></input>
+		  </div>
+ </div>
+ 
         </form>
-
+      
       </div>
-    </div>
-
-
-
-
+      </div>
+   
+ 
+      
+    
   );
 }
-
+ 
 export default SignUp;
