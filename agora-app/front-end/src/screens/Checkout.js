4f6323961a6a8eab51ac8904be32b1be05071dc1
husam <sam-788@hotmail.com>
2020-09-18 16:19:46 +1200

    updated checkout page

--- agora-app/front-end/src/screens/Checkout.js
+++ agora-app/front-end/src/screens/Checkout.js
@@ -24,99 +24,88 @@ import { detailListing } from '../actions/listingActions';
         
         return loading ? <div className="loading">Loading listing ...</div> :
         error? <div className="error"> {error} </div> :
-         <div className="checkout-container-body">
-         <div className="checkout-panel"> 
-                <div className="panel-body">        
-                <div className="title">
-                <h2>Checkout Form</h2>
-                </div>
- 
 
-    <div className="payment-method">
-      <label for="card" className="method card">
-        <div className="ship-logos">
-          <img src={pu} alt="delivery"/>
-        </div>
- 
-        <div className="radio-input">
-          <input id="card" type="radio" name="deliver"/>
-          Click for Pick-up
-        </div>
-      </label>
- 
-      <label for="cash" className="method cash">
-          <div className="container-cash">
-            <img src={dv} alt="pick-up"/>
-          </div>
-        <div className="radio-input">
-          <input id="cash" type="radio" name="deliver"/>
-          Click for Shipping
-        </div>
-      </label>
-    </div>
+   
+         <div className="checkout-container">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+ <h2 className="checkout-heading">Checkout Form</h2>
+<p></p>
+<div className="ch-row">
+  <div className="col-75">
+    <div className="ch-container">
+      <form>
+      
+        <div className="ch-row">
+          <div className="col-50">
+            <h3>Shipping Address</h3>
+            <label for="fname"><i className="fa fa-user"></i> Full Name</label>
+            <input type="text" id="fname" name="firstname" placeholder="First and Last Name"/>
+            <label for="email"><i className="fa fa-envelope"></i> Email</label>
+            <input type="text" id="email" name="email" placeholder="E-mail"/>
+            <label for="adr"><i className="fa fa-address-card-o"></i> Address</label>
+            <input type="text" id="adr" name="address" placeholder="111 11. Queens.st"/>
+            <label for="city"><i className="fa fa-institution"></i> City</label>
+            <input type="text" id="city" name="city" placeholder="Auckland"/>
 
-    <div className="payment-method">
-      <label for="card" className="method card">
-        <div className="card-logos">
-          <img src={visa_logo} alt="visa"/>
-         <img src={mastercard_logo} alt="mastercard"/>
-        </div>
- 
-        <div className="radio-input">
-          <input id="card" type="radio" name="payment"/>
-          Pay with credit card
-        </div>
-      </label>
- 
-      <label for="cash" className="method cash">
-          <div className="container-cash">
-            <img src={cash} alt="cash"/>
+            <div className="ch-row">
+              <div className="col-50">
+                <label for="state">Region</label>
+                <input type="text" id="state" name="state" placeholder="AUC"/>
+              </div>
+              <div className="col-50">
+                <label for="zip">Post Code</label>
+                <input type="text" id="zip" name="zip" placeholder="12345"/>
+              </div>
+            </div>
           </div>
-        <div className="radio-input">
-          <input id="cash" type="radio" name="payment"/>
-          Pay with Cash
-        </div>
-      </label>
-    </div>
- 
-    
 
-    
- 
-    <div className="input-fields">
-      <div className="column-1">
-        <label for="cardholder">Cardholder's Name</label>
-        <input type="text" id="cardholder" />
- 
-        <div className="small-inputs">
-          <div>
-            <label for="date">Expiry Date</label>
-            <input type="text" id="date" placeholder="MM / YY" />
+          <div className="col-50">
+            <h3>Payment</h3>
+			
+            <label for="fname">Accepted Cards</label>
+            <div className="icon-ch-container">
+              <i className="fa fa-cc-visa"></i>
+              <i className="fa fa-cc-amex"></i>
+              <i className="fa fa-cc-mastercard" ></i>
+              <i className="fa fa-cc-discover"></i>
+            </div>
+            <label for="cname">Name on Card</label>
+            <input type="text" id="cname" name="cardname" placeholder="Sam H Smith"/>
+            <label for="ccnum">Credit card number</label>
+            <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444"/>
+            <label for="expmonth">Exp Month</label>
+            <input type="text" id="expmonth" name="expmonth" placeholder="September"/>
+            <div className="ch-row">
+              <div className="col-50">
+                <label for="expyear">Exp Year</label>
+                <input type="text" id="expyear" name="expyear" placeholder="2024"/>
+              </div>
+              <div className="col-50">
+                <label for="cvv">CVV</label>
+                <input type="text" id="cvv" name="cvv" placeholder="798"/>
+              </div>
+            </div>
           </div>
- 
-          <div>
-            <label for="verification">CVV / CVC *</label>
-            <input type="password" id="verification"/>
-          </div>
+          
         </div>
- 
-      </div>
-      <div className="column-2">
-        <label for="cardnumber">Card Number</label>
-        <input type="text" id="cardnumber"/>
- 
-        <span className="info">* CVV or CVC is a unique security code found at the back of the card .</span>
-      </div>
+        <Link to={"/listing/"+ listing._id}>
+        <input type="submit" value="Cancel" className="ch-btn"/>
+         </Link>
+		<input type="submit" value="Proceed" className="ch-btn-c"/>
+       
+      </form>
     </div>
   </div>
-  
-   <div className="panel-footer">
-    <Link to={"/listing/"+ listing._id}>
-      <button className="btnc back-btn">Cancel</button>
-    </Link>
-    <button className="btnc next-btn">Proceed</button>
-   </div>        
-</div> 
+  <div className="col-25">
+    <div className="ch-container">
+      <h4>Product Image and price here </h4>
+      <p><a href="#">Product Listing</a> <span className="price">$</span></p>
+      
+      <hr></hr>
+      <p>Pay<span className="price"><b>$</b></span></p>
+    </div>
+  </div>
+</div>
             </div>              
     }
 
