3fbf4d515e9a7b48972b1b5b87f4a80ab55d7d5e
Hugo Baird <hugonzb@gmail.com>
2020-09-21 13:24:08 +1200

    Create listingComplete after checkout now working, needs testing

--- agora-app/front-end/src/screens/Checkout.js
+++ agora-app/front-end/src/screens/Checkout.js
@@ -14,7 +14,7 @@ import { createListingComplete } from '../actions/listingCompleteActions';
       const [sellerStudentid, setSellerStudentid] = useState("");
       const [buyerStudentid, setBuyerStudentid] = useState("");
       const [sellerName, setSellerName] = useState("");
-      const [buyerName, setBuyerName] = useState("");
+      const [buyerName, setBuyerName] = useState(userInfo.fname + " " + userInfo.sname);
       const [listingImage, setListingImage] = useState("");
       const [listingPrice, setListingPrice] = useState("");
       const [listingName, setListingName] = useState("");
@@ -23,7 +23,7 @@ import { createListingComplete } from '../actions/listingCompleteActions';
       const [buyerPostcode, setBuyerPostcode] = useState("");
       const [buyerRegion, setBuyerRegion] = useState("");
       const [sellerEmail, setSellerEmail] = useState("");
-      const [buyerEmail, setBuyerEmail] = useState("");
+      const [buyerEmail, setBuyerEmail] = useState(userInfo.email);
 
       useEffect(() => {
           dispatch(detailListing(props.match.params.id));
@@ -40,24 +40,8 @@ import { createListingComplete } from '../actions/listingCompleteActions';
         setListingPrice(listing.price);
         setListingName(listing.name);
         setSellerName(listing.seller);
-        setBuyerEmail(userInfo.email);
         setSellerEmail(listing.email);
-        setBuyerName(userInfo.fname + " " + userInfo.sname);
-        console.log(buyerStudentid);
-        console.log(sellerStudentid);
-        console.log(sellerName);
-        console.log(buyerName);
-        console.log(listingImage);
-        console.log(listingPrice);
-        console.log(listingName);
-        console.log(buyerAddress);
-        console.log(buyerCity);
-        console.log(buyerRegion);
-        console.log(buyerPostcode);
-        console.log(sellerEmail);
-        console.log(buyerEmail);
       }
-      
       const submitHandler = (e) => {
         e.preventDefault();
         if(listing){
@@ -97,9 +81,9 @@ import { createListingComplete } from '../actions/listingCompleteActions';
             <h3>Shipping Address</h3>
             <br></br>
             <label htmlFor="fname"><i className="fa fa-user"></i> Full Name</label>
-            <input type="text" id="fname" name="firstname" defaultValue={userInfo.fname + " " + userInfo.sname}/>
+            <input type="text" id="fname" name="firstname" defaultValue={userInfo.fname + " " + userInfo.sname} onChange={(e) => setBuyerName(e.target.value)}/>
             <label htmlFor="email"><i className="fa fa-envelope"></i> Email</label>
-            <input type="text" id="email" name="email" defaultValue={userInfo.email}/>
+            <input type="text" id="email" name="email" defaultValue={userInfo.email} onChange={(e) => setBuyerEmail(e.target.value)}/>
             <label htmlFor="adr"><i className="fa fa-address-card-o"></i> Address</label>
             <input type="text" id="adr" name="address" placeholder="Delivery address" required onInput={setFields} onChange={(e) => setBuyerAddress(e.target.value)}/>
             <label htmlFor="city"><i className="fa fa-institution"></i> City</label>
