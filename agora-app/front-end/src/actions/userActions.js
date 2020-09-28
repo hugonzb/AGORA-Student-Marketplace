47031c021bd45f7bda5cf2c8a4e6f9b4981b74f1
stece850 <stece850@student.otago.ac.nz>
2020-09-28 17:49:42 +1300

    added PUT method for updating a user in actions

--- agora-app/front-end/src/actions/userActions.js
+++ agora-app/front-end/src/actions/userActions.js
@@ -1,53 +1,142 @@
 import Axios from "axios";
-import Cookie from 'js-cookie';
-import { 
-    USER_SIGNUP_FAIL, 
-    USER_SIGNUP_REQUEST, 
-    USER_SIGNUP_SUCCESS, 
-    USER_SIGNIN_FAIL, 
-    USER_SIGNIN_REQUEST, 
-    USER_SIGNIN_SUCCESS, 
-    USER_LOGOUT
-    USER_UPDATE_REQUEST, 
-    USER_UPDATE_SUCCESS, 
-    USER_UPDATE_FAIL, 
+import Cookie from "js-cookie";
+import {
+  USER_SIGNUP_FAIL,
+  USER_SIGNUP_REQUEST,
+  USER_SIGNUP_SUCCESS,
+  USER_SIGNIN_FAIL,
+  USER_SIGNIN_REQUEST,
+  USER_SIGNIN_SUCCESS,
+  USER_LOGOUT,
+  USER_UPDATE_REQUEST,
+  USER_UPDATE_SUCCESS,
+  USER_UPDATE_FAIL,
 } from "../constants/userConstants";
 
-const signUp = (studentid, fname, sname, password, email,
-    dob, gender, university, street_address, city, postcode, profilePicture ) => async (dispatch) => {
-        dispatch({
-            type: USER_SIGNUP_REQUEST, payload: {studentid, fname, sname, password, email,
-                dob, gender, university, street_address, city, postcode, profilePicture } });
-    try {
-        const { data } = await Axios.post("/api/users/signup", {studentid, fname, sname, password, email,
-            dob, gender, university, street_address, city, postcode, profilePicture });
-        dispatch({type:USER_SIGNUP_SUCCESS, payload: data});
-    }catch(error){
-        dispatch({type: USER_SIGNUP_FAIL, payload: error.message});
-    }
-}
- 
+const signUp = (
+  studentid,
+  fname,
+  sname,
+  password,
+  email,
+  dob,
+  gender,
+  university,
+  street_address,
+  city,
+  postcode,
+  profilePicture
+) => async (dispatch) => {
+  dispatch({
+    type: USER_SIGNUP_REQUEST,
+    payload: {
+      studentid,
+      fname,
+      sname,
+      password,
+      email,
+      dob,
+      gender,
+      university,
+      street_address,
+      city,
+      postcode,
+      profilePicture,
+    },
+  });
+  try {
+    const { data } = await Axios.post("/api/users/signup", {
+      studentid,
+      fname,
+      sname,
+      password,
+      email,
+      dob,
+      gender,
+      university,
+      street_address,
+      city,
+      postcode,
+      profilePicture,
+    });
+    dispatch({ type: USER_SIGNUP_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({ type: USER_SIGNUP_FAIL, payload: error.message });
+  }
+};
+
 const signIn = (email, password) => async (dispatch) => {
-    dispatch({type: USER_SIGNIN_REQUEST, payload: {email, password}});
-    try{
-        const { data } = await Axios.post("/api/users/signin", {email, password});
-        dispatch({type:USER_SIGNIN_SUCCESS, payload: data});
-        Cookie.set('userInfo', JSON.stringify(data));
-    }catch(error){
-        dispatch({type:USER_SIGNIN_FAIL, payload: error.message});
-    }
-} 
+  dispatch({ type: USER_SIGNIN_REQUEST, payload: { email, password } });
+  try {
+    const { data } = await Axios.post("/api/users/signin", { email, password });
+    dispatch({ type: USER_SIGNIN_SUCCESS, payload: data });
+    Cookie.set("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({ type: USER_SIGNIN_FAIL, payload: error.message });
+  }
+};
 
 const logout = () => (dispatch) => {
-    alert("Successfully logged out");
-    Cookie.remove("userInfo");
-    dispatch({type:USER_LOGOUT});
-}
+  alert("Successfully logged out");
+  Cookie.remove("userInfo");
+  dispatch({ type: USER_LOGOUT });
+};
 
 const removeUser = () => (dispatch) => {
-    dispatch({type:USER_LOGOUT});
-}
+  dispatch({ type: USER_LOGOUT });
+};
 
+const updateUser = (
+  studentid,
+  fname,
+  sname,
+  password,
+  email,
+  dob,
+  gender,
+  university,
+  street_address,
+  city,
+  postcode,
+  profilePicture
+) => async (dispatch) => {
+  dispatch({
+    type: USER_UPDATE_REQUEST,
+    payload: {
+      studentid,
+      fname,
+      sname,
+      password,
+      email,
+      dob,
+      gender,
+      university,
+      street_address,
+      city,
+      postcode,
+      profilePicture,
+    },
+  });
+  try {
+    const { data } = await axios.put("/api/users/" + studentid, {
+      studentid,
+      fname,
+      sname,
+      password,
+      email,
+      dob,
+      gender,
+      university,
+      street_address,
+      city,
+      postcode,
+      profilePicture,
+    });
+    dispatch({ type: USER_UPDATE_REQUEST, payload: data });
+    console.log("dispatch succesfull log");
+  } catch (error) {
+    dispatch({ type: USER_UPDATE_FAIL, payload: error.message });
+  }
+};
 
-export { signUp, signIn, logout, removeUser}
-
+export { signUp, signIn, logout, removeUser, updateUser };
