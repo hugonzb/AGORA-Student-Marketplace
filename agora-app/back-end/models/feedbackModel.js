2d94151f04fc0d0b5c503371ede069c345527209
Leon <le.o.n@outlook.com>
2020-09-30 19:06:14 +1300

    Have finished feedbackModel.js

--- agora-app/back-end/models/feedbackModel.js
+++ agora-app/back-end/models/feedbackModel.js
@@ -3,8 +3,12 @@ import mongoose, { Mongoose } from 'mongoose';
 // DB schema for feedback submission from user:
 
 const feedbackSchema = new mongoose.Schema({
-    userID: {type: String, required: false},
-    
+    name: {type: String, required: false},
+    email: { type: String, required: false},
+    feedback: { type: String, required: true}
 },{
     timestamps: true,
 },{strict: true});
+
+const feedbackModel = mongoose.model("Feedback", feedbackSchema);
+export default feedbackModel;
