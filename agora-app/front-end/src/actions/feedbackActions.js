9056b0e53043a4acb8d13778bd1fdfaca6be7851
Leon <le.o.n@outlook.com>
2020-10-02 22:43:08 +1300

    fixed error in feedbackActions.js export call

--- agora-app/front-end/src/actions/feedbackActions.js
+++ agora-app/front-end/src/actions/feedbackActions.js
@@ -1,20 +1,22 @@
 import axios from 'axios';
-import {FEEDBACK_POST_SUCCESS, 
-        FEEDBACK_POST_REQUEST, 
-        FEEDBACK_POST_FAIL} from '../constants/feedbackConstants';
+import {
+    FEEDBACK_POST_SUCCESS,
+    FEEDBACK_POST_REQUEST,
+    FEEDBACK_POST_FAIL
+} from '../constants/feedbackConstants';
 
-const submitFeedback = (name, email, feedback) => async (dispatch) =>{
+const submitFeedback = (name, email, feedback) => async (dispatch) => {
     dispatch({
-        type: FEEDBACK_POST_REQUEST, payload: {name, email, feedback}
+        type: FEEDBACK_POST_REQUEST, payload: { name, email, feedback }
     });
-    try{
-        const {data} = await axios.post("/api/feedback/postfeedback", {
+    try {
+        const { data } = await axios.post("/api/feedback/postfeedback", {
             name, email, feedback
         });
-        dispatch({type: FEEDBACK_POST_SUCCESS, payload: data});
-    }catch(error){
-        dispatch({type: FEEDBACK_POST_FAIL, payload: error.message});
+        dispatch({ type: FEEDBACK_POST_SUCCESS, payload: data });
+    } catch (error) {
+        dispatch({ type: FEEDBACK_POST_FAIL, payload: error.message });
     }
 };
 
-export submitFeedback;
+export {submitFeedback};
