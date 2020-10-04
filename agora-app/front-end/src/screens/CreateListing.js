a1daa98b51d1df3486944ada59951bcb095c9dc5
Vainui Moresi <zedricvm@gmail.com>
2020-10-04 21:08:47 +1300

    Changed the box borders from red to black

--- agora-app/front-end/src/screens/CreateListing.js
+++ agora-app/front-end/src/screens/CreateListing.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { createListing } from "../actions/listingActions";
 import Axios from "axios";
-import "../signup.css"; 
+import "../createListing.css"; 
 
 function CreateListing(props) {
   /* 
