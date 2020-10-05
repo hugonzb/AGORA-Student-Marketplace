1fa04a267986981f4a186dd33fabc8e1b3189894
stece850 <stece850@student.otago.ac.nz>
2020-10-06 12:05:08 +1300

    added member photo and role

--- agora-app/front-end/src/screens/About.js
+++ agora-app/front-end/src/screens/About.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import "../signup.css";
-import {submitFeedback} from "../actions/feedbackActions";
+import { submitFeedback } from "../actions/feedbackActions";
 import checkmark from "../images/check-mark.png";
 
 function About(props) {
-
   const dispatch = useDispatch();
 
   const [name, setName] = useState("");
@@ -13,6 +12,7 @@ function About(props) {
   const [feedback, setFeedback] = useState("");
   const hugoicon = "../profilePictures/hugo (2)_1601868657464.jpg";
   const leonicon = "../profilePictures/LeonAbout.png";
+  const cedricicon = "../profilePictures/cedricabout.jpg";
   const [uploading] = useState(false);
 
   useEffect(() => {
@@ -30,7 +30,6 @@ function About(props) {
   };
 
   return (
-
     <div className="about-container">
       <h1 className="heading">Meet The Agora Team</h1>
 
@@ -57,9 +56,9 @@ function About(props) {
         </div>
 
         <div className="profile-card">
-          <img src={""} className="profile-img" alt="profile" />
-          <h3 className="user-name">Name</h3>
-          <h5>Memeber Role</h5>
+          <img src={cedricicon} className="profile-img" alt="profile" />
+          <h3 className="user-name">Cedric</h3>
+          <h5>Back-end Developer</h5>
           <p></p>
         </div>
 
@@ -69,39 +68,47 @@ function About(props) {
           <h5>Memeber Role</h5>
           <p></p>
         </div>
-
       </div>
       <div className="about-help-box">
-        <div className="about-help">Need help?</div> 
-        <a href={checkmark} download="Agora_Help_Documentation">Download the end-user documentation here.</a>
+        <div className="about-help">Need help?</div>
+        <a href={checkmark} download="Agora_Help_Documentation">
+          Download the end-user documentation here.
+        </a>
       </div>
       <div className="about-feedback">
         <form onSubmit={submitHandler}>
-          <div className="contact-comment"> <h2> Leave us a Comment </h2>	</div>
-          <input name="namess" type="text" className="feedback-input" placeholder="Name" onChange={(e) => setName(e.target.value)}/>
-          <input name="emailss" type="text" className="feedback-input" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
-          <textarea type="text" name="texts" className="feedback-input" placeholder="Comment" required onChange={(e) => setFeedback(e.target.value)}></textarea>
+          <div className="contact-comment">
+            {" "}
+            <h2> Leave us a Comment </h2>{" "}
+          </div>
+          <input
+            name="namess"
+            type="text"
+            className="feedback-input"
+            placeholder="Name"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            name="emailss"
+            type="text"
+            className="feedback-input"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <textarea
+            type="text"
+            name="texts"
+            className="feedback-input"
+            placeholder="Comment"
+            required
+            onChange={(e) => setFeedback(e.target.value)}
+          ></textarea>
           {uploading && <div>Submitting feedback</div>}
           <input type="submit" name="send it" value="Send" />
         </form>
       </div>
     </div>
-
-
-
-
   );
 }
 
 export default About;
-
-
-
-
-
-
-
-
-
-
-
